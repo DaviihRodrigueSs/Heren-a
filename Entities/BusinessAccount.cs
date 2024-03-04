f1c0c4a0ccbe35ff3d543e36c782cb8f8a59268e
DaviihRodrigueSs <davi.devrodrigues@gmail.com>
2024-03-04 18:25:43 -0300

    Herença

---
+++ Entities/BusinessAccount.cs
@@ -0,0 +1,28 @@
+﻿using Herança.Entities;
+using System;
+namespace Herança.Entities
+{
+    internal class BusinessAccount : Account //herença
+    {
+        public double LoanLimit { get; set; }
+        public BusinessAccount()
+        {
+          
+        }
+
+        public BusinessAccount(int number, string holder, double balance, double loanLimit)
+            : base(number, holder, balance)
+        {
+            LoanLimit = loanLimit;
+        }
+        public void Loan(double amount)
+        {
+            if (amount <= LoanLimit)
+            {
+                Balance += amount;
+
+            }
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿
+using Herança.Entities;
+using System;
+
+namespace Herança
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            BusinessAccount account = new BusinessAccount(8010, "Bob Draw", 100.0, 500.0);
+
+            Console.WriteLine(account.Balance);
+
+            // não tem como alterar por causa do protected account.Balance = 200.00
+        }
+    }
+}
